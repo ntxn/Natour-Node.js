435c90d625a8313cc5d129f19c29fd7d1c621d6f
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-02-18 14:58:33 -0800

    Created simple CRUD operations to get and update data from local files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A dynamic web application built to learn and experience server side web development using Node.js, Express, MongoDB
 
 - Using Express for routing and making Http requests
+  - Step 1: create simple APIs (updating local files for now) using RESTful architecture ==> CRUD Operations created
--- app.js
+++ app.js
@@ -1,16 +1,110 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
-});
+app.use(express.json());
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: { tours }
+  });
+};
+
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      messsage: 'Invalid ID'
+    });
+  }
+  const tour = tours.find(e => e.id === id);
+
+  res.status(200).json({
+    status: 'Success',
+    data: { tour }
+  });
+};
+
+const createTour = (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      if (err)
+        res.status(404).json({
+          status: 'Fail'
+        });
+      else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour
+          }
+        });
+      }
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      messsage: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      messsage: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
