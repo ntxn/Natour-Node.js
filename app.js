4255708477bf620ba29c92215075e04ffbef1e00
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-02-18 15:30:04 -0800

    Implemented simple Users Routes

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const fs = require('fs');
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1. MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2. ROUTE HANDLERS
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'Success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: { tours }
   });
@@ -89,11 +104,43 @@ const deleteTour = (req, res) => {
   });
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+// USERS ROUTE HANDLERS
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    messsage: 'This route is not yet defined'
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    messsage: 'This route is not yet defined'
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    messsage: 'This route is not yet defined'
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    messsage: 'This route is not yet defined'
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    messsage: 'This route is not yet defined'
+  });
+};
+
+// 3. ROUTES
 
 app
   .route('/api/v1/tours')
@@ -106,6 +153,18 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// 4. START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
